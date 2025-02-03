dd7c86d17961992f5d83078e70863c092a93e1ca
Dwip Sarker <email.dwip@gmail.com>
2025-02-03 18:12:38 +0600

    category selector added into portfolio section

--- src/components/ui/SectionIntro.jsx
+++ src/components/ui/SectionIntro.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function SectionIntro({ title, subtitle }) {
   return (
-    <div className="mb-10 text-center">
+    <div className="mb-6 text-center">
       <h2 className="mb-1 font-semibold text-[#23343F] lg:text-lg">{title}</h2>
       <h3 className="text-2xl font-semibold lg:text-4xl">{subtitle}</h3>
     </div>
