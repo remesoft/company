2e43ae48bba228487e10ac82ce60861387e36069
Dwip Sarker <email.dwip@gmail.com>
2025-02-04 10:41:46 +0600

    final ui update on services section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -6,10 +6,10 @@ import servicesData from "../data/services";
 
 export default function Services() {
   return (
-    <section className="py-16 lg:py-36">
-      <div className="mx-auto max-w-[1200px] px-4 xl:px-0">
+    <section className="section-padding">
+      <div className="section-margin">
         <SectionIntro title="Our Services" subtitle="Services provides by us" />
-        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
+        <div className="mt-8 grid gap-4 md:grid-cols-2 lg:mt-12 lg:grid-cols-3 xl:grid-cols-3">
           {servicesData.map((item, index) => (
             <ServicesCard
               key={index}
--- src/components/ui/SectionIntro.jsx
+++ src/components/ui/SectionIntro.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 export default function SectionIntro({ title, subtitle }) {
   return (
-    <div className="mb-16 text-center">
+    <div className="text-center">
       <h2 className="mb-1 font-semibold text-[#23343F] lg:text-lg">{title}</h2>
-      <h3 className="text-2xl font-semibold lg:text-4xl">{subtitle}</h3>
+      <h3 className="text-2xl font-semibold text-slate-700 lg:text-4xl">
+        {subtitle}
+      </h3>
     </div>
   );
 }
